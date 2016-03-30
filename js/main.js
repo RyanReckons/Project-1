c6111f12c37f46cf0a911f8080b513201768521a
Ryan Rosenlund <rrosenlund@live.com>
2016-03-30 16:46:51 -0600

    added first button in js

--- js/main.js
+++ js/main.js
@@ -1,7 +1,33 @@
-function ellipse(x, y, width, height){
-	
+var shortPosAnswers = ['yes', 'sure', 'Sure', 'yep', 'yepper'];
+var longPosAnswers = ['lets do it', 'im all for it', 'you betcha', 'sure thing'];
+var negAnswers = ['not sure', 'not so good', 'not so', 'no thanks', 'no', 'not really'];
+var answer1 = ['12am', '1am', '2am', '3am', '4am', '5am'];
+var answer2 = [, '6am', '7am'];
+var answer3 = ['8am', '9am', '10am','11am'];
+var posGreet = ['good', 'great thank you!', 'great'];
+
+function playBall(){
+  var ball,
+      typeOfBall;
+
+  ball = prompt("Want to play ball, yes or no?");
+  ball = ball.toLowerCase();
+  
+  if (shortPosAnswers.indexOf(ball) >= 0){
+      typeOfBall = prompt("Basketball or Baseball?");
+      typeOfBall = typeOfBall.toLowerCase();
+      if (typeOfBall == "basketball"){
+          game = prompt("One on One or Horse?")
+          game = game.toLowerCase();
+          if (game == "one on one"){
+              confirm("Let's flip a coin to see who gets ball first!");
+          } else {
+              confirm("You go first!");
+          }
+      } else if (typeOfBall == "baseball"){
+          confirm("Let's play catch!");
+      }
+  } else {
+      confirm("Maybe next time then!");
+  }
 }
-// bottom circle:
-ellipse(200, 300, 150, 150);
-// middle circle:
-ellipse(200, 200, 100, 100);
